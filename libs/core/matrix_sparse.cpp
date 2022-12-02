7e0c9e0c0556d35c3f9aab4b91a151e603a0840d
bevan <jnsbev005@myuct.ac.za>
2022-12-02 22:11:24 +0100

    - Moved all containers, associated operators, and their tests into core library. - Added disa.h (library header), and updated the CMakeLists.txt accordingly. - Solver library now linked with disa.

--- libs/core/matrix_sparse.cpp
+++ libs/core/matrix_sparse.cpp
@@ -15,10 +15,10 @@
 //  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 //  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ----------------------------------------------------------------------------------------------------------------------
-//  File Name: matrix_sparse.cpp
-//  Description: Implements the definitions for out-of-line declarations for member and non-member functions relating
-//               to sparse matrices.
+//  File Name:
+//  Description:
 // ----------------------------------------------------------------------------------------------------------------------
+//
 
 #include <tuple>
 #include "matrix_sparse.h"
