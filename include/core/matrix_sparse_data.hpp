6c24fb3927c18e40fa1811cd0ee9db4763fe4c48
bevanwsjones <bevanwsjones@gmail.com>
2024-10-04 10:50:12 +0200

    - updated doc comments for consistency

--- include/core/matrix_sparse_data.hpp
+++ include/core/matrix_sparse_data.hpp
@@ -32,15 +32,13 @@
 namespace Disa {
 
 /**
- * @brief A structure to represent Compressed Sparse Row (CSR) data format.
+ * @struct CSR_Data
+ * @brief Represents a sparse matrix in Compressed Sparse Row (CSR) format.
  * 
- * This structure is used to store sparse matrices in CSR format, it contains only the data and no operations. The data
- * is stored in three standard vectors: row_offset, column_index, and value. The row_offset vector stores the index of
- * the first non-zero element in each row, the column_index vector stores the column index of each non-zero element, and
- * the value vector stores the value of each non-zero element. The columns member variable stores the number of columns
- * in the matrix. 
- * 
- * The class is not intended for use outside of the Matrix_Sparse class which wraps it.
+ * This structure stores sparse matrices in CSR format, containing only the data and no operations. It uses three
+ * standard vectors: row_offset, i_column, and value to store the matrix data. The 'columns' member variable stores the
+ * number of columns in the matrix. This class is not intended for use outside of the Matrix_Sparse class which wraps 
+ * it.
  * 
  * @tparam _value_type The type of the values stored in the matrix.
  * @tparam _index_type The type of the indices used for rows and columns, defaults to std::size_t.
@@ -68,15 +66,15 @@ struct CSR_Data {
  *
  * @tparam _value_type The type of the values stored in the CSR data structure.
  * @tparam _index_type The type of the indices used in the CSR data structure.
- * @param data A reference to the CSR_Data object containing the matrix data.
- * @param iter_row An iterator pointing to a specific row in the row offset vector.
+ * @param[in] data A reference to the CSR_Data object containing the matrix data.
+ * @param[in] iter_row An iterator pointing to a specific row in the row offset vector.
  * @return The row index corresponding to the given iterator.
  * 
- * @note assumes iterator is in rage begin() - end().
+ * @note Assumes iterator is in range [begin(), end()).
  */
 template<typename _value_type, typename _index_type>
-_index_type i_row(const CSR_Data<_value_type, _index_type>& data,
-                  const typename std::vector<_index_type>::const_iterator& iter_row) noexcept {
+[[nodiscard]] _index_type i_row(const CSR_Data<_value_type, _index_type>& data,
+                                const typename std::vector<_index_type>::const_iterator& iter_row) noexcept {
   return static_cast<_index_type>(std::distance(data.row_offset.cbegin(), iter_row));
 };
 
@@ -85,15 +83,15 @@ _index_type i_row(const CSR_Data<_value_type, _index_type>& data,
  *
  * @tparam _value_type The type of the values stored in the CSR data structure.
  * @tparam _index_type The type of the indices used in the CSR data structure.
- * @param data A reference to the CSR_Data object containing the matrix data.
- * @param iter_row An iterator pointing to a specific row in the row offset vector.
+ * @param[in] data A reference to the CSR_Data object containing the matrix data.
+ * @param[in] iter_row An iterator pointing to a specific row in the row offset vector.
  * @return The row index corresponding to the given iterator.
  * 
- * @note assumes iterator is in rage begin() - end().
+ * @note Assumes iterator is in range [begin(), end()).
  */
 template<typename _value_type, typename _index_type>
-_index_type i_row(CSR_Data<_value_type, _index_type>& data,
-                  const typename std::vector<_index_type>::iterator& iter_row) noexcept {
+[[nodiscard]] _index_type i_row(CSR_Data<_value_type, _index_type>& data,
+                                const typename std::vector<_index_type>::iterator& iter_row) noexcept {
   return i_row(static_cast<const CSR_Data<_value_type, _index_type>&>(data), iter_row);
 };
 
@@ -105,7 +103,7 @@ _index_type i_row(CSR_Data<_value_type, _index_type>& data,
  *
  * @tparam _value_type The type of the values stored in the CSR matrix.
  * @tparam _index_type The type of the indices used in the CSR matrix.
- * @param data The CSR_Data structure containing the matrix data.
+ * @param[in] data The CSR_Data structure containing the matrix data.
  * @return The number of rows in the CSR matrix.
  */
 template<typename _value_type, typename _index_type>
@@ -118,8 +116,8 @@ _index_type size_row(const CSR_Data<_value_type, _index_type>& data) noexcept {
  * 
  * @tparam _value_type The type of values stored in the CSR data structure.
  * @tparam _index_type The type of indices used in the CSR data structure.
- * @param data The CSR data structure containing the matrix data.
- * @return _index_type The number of columns in the CSR data structure.
+ * @param[in] data The CSR data structure containing the matrix data.
+ * @return The number of columns in the CSR data structure.
  */
 template<typename _value_type, typename _index_type>
 _index_type size_column(const CSR_Data<_value_type, _index_type>& data) noexcept {
@@ -131,7 +129,7 @@ _index_type size_column(const CSR_Data<_value_type, _index_type>& data) noexcept
  *
  * @tparam _value_type The type of the values stored in the CSR data structure.
  * @tparam _index_type The type of the indices used in the CSR data structure.
- * @param data The CSR data structure containing the sparse matrix data.
+ * @param[in] data The CSR data structure containing the sparse matrix data.
  * @return The number of non-zero elements in the CSR data structure.
  */
 template<typename _value_type, typename _index_type>
@@ -142,16 +140,15 @@ _index_type size_non_zero(const CSR_Data<_value_type, _index_type>& data) noexce
 /**
  * @brief Resizes the CSR data structure to represent a new specified number of rows and columns.
  *
- * This function adjusts the size of the CSR data structure, which resizing the row offsets, column indices, and values.
- * If the new size is smaller than the current size, the function will truncate the data accordingly. Specifically for 
- * column truncation, the function will loop over each row to remove any columns that exceed the new column size.
+ * This function adjusts the size of the CSR data structure by resizing the row offsets, column indices, and values. If 
+ * the new size is smaller than the current size, the function will truncate the data accordingly.
  *
  * @tparam _value_type The type of the values stored in the CSR data structure.
  * @tparam _index_type The type of the indices used in the CSR data structure.
  * @tparam _arg_index_type The type of the arguments used for resizing, which must be convertible to _index_type.
- * @param data The CSR data structure to be resized.
- * @param row The new number of rows, must be non-negative.
- * @param column The new number of columns, must be non-negative.
+ * @param[in,out] data The CSR data structure to be resized.
+ * @param[in] row The new number of rows, must be non-negative.
+ * @param[in] column The new number of columns, must be non-negative.
  */
 template<typename _value_type, typename _index_type, typename _arg_index_type>
 std::enable_if<std::is_convertible_v<_arg_index_type, _index_type>, void>::type resize(
@@ -198,17 +195,16 @@ CSR_Data<_value_type, _index_type>& data, const _arg_index_type& row, const _arg
  * @brief Inserts a value into a CSR data structure.
  *
  * This function inserts a value into the specified position (row, column) of the CSR_Data structure. If the position 
- * already contains a value, the function returns without inserting. If the row or column index is out of bounds, the
- * function resizes the data structure accordingly (much in the same way an insert into a map would add a new key-value
- * pair).
+ * already contains a value, the function returns without inserting. If the row or column index is out of bounds, the 
+ * function resizes the data structure accordingly.
  *
  * @tparam _value_type The type of the values stored in the matrix.
  * @tparam _index_type The type of the indices used in the CSR_Data structure.
  * @tparam _arg_index_type The type of the row and column indices provided as arguments.
- * @param data The CSR_Data structure representing the sparse matrix.
- * @param row The row non-negative index where the value should be inserted.
- * @param column The column non-negative index where the value should be inserted.
- * @param value The value to be inserted into the matrix.
+ * @param[in,out] data The CSR_Data structure representing the sparse matrix.
+ * @param[in] row The row non-negative index where the value should be inserted.
+ * @param[in] column The column non-negative index where the value should be inserted.
+ * @param[in] value The value to be inserted into the matrix.
  * @return A pair consisting of an iterator to the inserted position and a boolean indicating
  *         whether the insertion was successful (true) or if the position already contained a value (false).
  */
@@ -244,17 +240,16 @@ std::pair<typename CSR_Data<_value_type, _index_type>::iterator, bool> insert(CS
 /**
  * @brief Inserts a new element or assigns to an existing element in the CSR data structure.
  * 
- * This function either inserts a new element into the CSR data structure if the specified
- * row and column do not exist, or assigns a new value to an existing element. If the row
- * or column exceed the current dimensions of the matrix, the matrix is resized accordingly.
+ * This function either inserts a new element into the CSR data structure if the specified row and column do not exist,
+ * or assigns a new value to an existing element.
  * 
  * @tparam _value_type The type of the values stored in the matrix.
  * @tparam _index_type The type of the indices used in the matrix.
  * @tparam _arg_index_type The type of the argument indices (must be convertible to _index_type).
- * @param data The CSR data structure to modify.
- * @param row The row index of the element to insert or assign.
- * @param column The column index of the element to insert or assign.
- * @param value The value to insert or assign.
+ * @param[in,out] data The CSR data structure to modify.
+ * @param[in] row The row index of the element to insert or assign.
+ * @param[in] column The column index of the element to insert or assign.
+ * @param[in] value The value to insert or assign.
  * @return A pair consisting of an iterator to the inserted or found position and a boolean indicating
  *         whether the insertion was successful (true) or the value was assigned (false).
  */
@@ -271,19 +266,17 @@ const _value_type& value) {
  * @brief Finds the lower bound of a specified element position in a CSR data structure.
  *
  * This function searches for the lower bound of a specified element position in a CSR data structure. It returns an 
- * iterator to the first element that is not less than the specified row and column indices, in must the same way as a 
- * map. If no element is found, the function returns an iterator to the end of the 'row', such that insert maybe used to
- * insert into the row. 
+ * iterator to the first element that is not less than the specified row and column indices.
  *
  * @tparam _value_type The type of the values stored in the matrix.
  * @tparam _index_type The type of the indices used in the matrix.
  * @tparam _arg_index_type The type of the argument indices.
- * @param data The CSR matrix data.
- * @param row The row non-negative index to search for.
- * @param column The column non-negative index to search for.
+ * @param[in] data The CSR matrix data.
+ * @param[in] row The row non-negative index to search for.
+ * @param[in] column The column non-negative index to search for.
  * @return A CSR Data iterator to the row, column, and value of the found element. If not found, returns end iterators.
  * 
- * @warning A simple comparison with end() for each iterator, in the returned tuple iterator, is not sufficient to check 
+ * @warning A simple comparison with end() for each iterator in the returned tuple iterator is not sufficient to check 
  *          if the element was not found. The iterators must be checked on a 'per row' basis.
  */
 template<typename _value_type, typename _index_type, typename _arg_index_type>
@@ -308,19 +301,17 @@ lower_bound(const CSR_Data<_value_type, _index_type>& data, const _arg_index_typ
  * @brief Finds the lower bound of a specified element position in a CSR data structure.
  *
  * This function searches for the lower bound of a specified element position in a CSR data structure. It returns an 
- * iterator to the first element that is not less than the specified row and column indices, in must the same way as a 
- * map. If no element is found, the function returns an iterator to the end of the 'row', such that insert maybe used to
- * insert into the row. 
+ * iterator to the first element that is not less than the specified row and column indices.
  *
  * @tparam _value_type The type of the values stored in the matrix.
  * @tparam _index_type The type of the indices used in the matrix.
  * @tparam _arg_index_type The type of the argument indices.
- * @param data The CSR matrix data.
- * @param row The row non-negative index to search for.
- * @param column The column non-negative index to search for.
+ * @param[in] data The CSR matrix data.
+ * @param[in] row The row non-negative index to search for.
+ * @param[in] column The column non-negative index to search for.
  * @return A CSR Data iterator to the row, column, and value of the found element. If not found, returns end iterators.
  * 
- * @warning A simple comparison with end() for each iterator, in the returned tuple iterator, is not sufficient to check 
+ * @warning A simple comparison with end() for each iterator in the returned tuple iterator is not sufficient to check 
  *          if the element was not found. The iterators must be checked on a 'per row' basis.
  */
 template<typename _value_type, typename _index_type, typename _arg_index_type>
